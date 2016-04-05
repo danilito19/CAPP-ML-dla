26bd862d0986657a487fe504d835cd3208d09c75
danilito19 <danalitovsky@gmail.com>
2016-04-05 15:51:59 -0500

    some work on ml workflow

--- workflow.py
+++ workflow.py
@@ -1,23 +1,35 @@
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
+import pylab
 import requests
 import json
+import sys
+from sklearn.linear_model import LogisticRegression
+
+plt.rcParams["figure.figsize"] = [18.0, 8.0]
+plt.style.use('ggplot')
 
 def read_data(file):
     '''
     Read in data and return a pandas df
     '''
-    pass
+    return pd.read_csv(file_name, header=0)
 
 def print_statistics(data):
     '''
     Print statistics, correlation
     '''
-    pass
+    print '****column names:  ', data.columns.values
+    print '****top of the data: ', data.head()
+    print '****dataframe shape: ', data.shape
+    print '****statistics: ', data.describe(include='all')
+    print '****sum of null values by column: ', data.isnull().sum()
+    print '****correlation matrix: ', data.corr()
 
 def visualize(data):
-    pass
+    data.hist()
+    pylab.show()
 
 def model_data(data):
     pass
@@ -27,17 +39,24 @@ def evaluate_model(data):
 
 
 if __name__=="__main__":
-    instructions = '''Usage: workflow file name '''
+    instructions = '''Usage: python workflow.py filename '''
 
 
     if(len(sys.argv) != 2):
         print(instructions)
         sys.exit()
 
-    file_name = sys.argv[2]
+
+    file_name = sys.argv[1]
 
     try:
-        pass
+        df = read_data(file_name)
+        print_statistics(df)
+        visualize(df)
+        #create train / test data
+
+    #if output to file: output all to file
+
         
     except Exception, e:
         print('Error: ', e)
