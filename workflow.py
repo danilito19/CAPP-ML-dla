50524a5d1d578cb092e016b1c74766a5460d7781
Dani <danalitovsky@gmail.com>
2016-04-06 11:27:48 -0500

    some work workflow

--- workflow.py
+++ workflow.py
@@ -8,7 +8,7 @@ import sys
 from sklearn.linear_model import LogisticRegression
 
 plt.rcParams["figure.figsize"] = [18.0, 8.0]
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
 
 def read_data(file):
     '''
@@ -20,16 +20,35 @@ def print_statistics(data):
     '''
     Print statistics, correlation
     '''
-    print '****column names:  ', data.columns.values
-    print '****top of the data: ', data.head()
-    print '****dataframe shape: ', data.shape
-    print '****statistics: ', data.describe(include='all')
-    print '****sum of null values by column: ', data.isnull().sum()
-    print '****correlation matrix: ', data.corr()
+    pd.set_option('display.width', 20)
+    print '****column names:  ', "\n", data.columns.values
+    print '****top of the data: ', "\n",  data.head()
+    print '****dataframe shape: ', "\n", data.shape
+    print '****statistics: ', "\n", data.describe(include='all')
+    print '****sum of null values by column: ', "\n", data.isnull().sum()
+    print '****correlation matrix: ', "\n", data.corr()
 
 def visualize(data):
     data.hist()
-    pylab.show()
+    plt.savefig('hist.jpg')
+
+def impute_missing(data):
+    '''
+    Find columns with missing data and impute with the column's mean.
+    '''
+
+    headers = list(data.columns)
+    for name in headers:
+        if data[name].isnull().values.any():
+            data[name] = data[name].fillna(data[name].mean())
+
+    assert not data.isnull().values.any()
+
+def generate_categorical_features(data):
+    pass
+
+def generate_cont_features(data):
+    pass
 
 def model_data(data, X, y):
     
@@ -45,12 +64,14 @@ def go(file_name):
     run file
     '''
     df = read_data(file_name)
-    print_statistics(df)
-    visualize(df)
+    # print_statistics(df)
+    # visualize(df)
+    impute_missing(df)
 
-    X = ['Age', 'GPA', 'Days_missed']
-    y = 'Gender'
-    model_data(data, X, y)
+
+    # X = ['Age', 'GPA', 'Days_missed']
+    # y = 'Gender'
+    #model_data(data, X, y)
     #create train / test data
 
 if __name__=="__main__":
