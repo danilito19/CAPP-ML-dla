4caea7dba2bd0787759f654da868155536f19662
danilito19 <danalitovsky@gmail.com>
2016-04-11 16:29:20 -0500

    more work

--- workflow.py
+++ workflow.py
@@ -8,6 +8,7 @@ import random
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 plt.rcParams["figure.figsize"] = [18.0, 8.0]
 
@@ -79,35 +80,58 @@ def log_column(data, column):
     Log the values of a column.
 
     Good to use when working with income data
+
+    Returns the name of the new column to include programmatically in features
     '''
 
     log_col = 'log_' + str(column)
     data[log_col] = data[column].apply(lambda x: np.log(x + 1))
 
-def generate_categorical_features(data):
-    pass
-    #df.newcolumn = 1
-    #https://github.com/yhat/DataGotham2013/blob/master/notebooks/7%20-%20Feature%20Engineering.ipynb
+    return log_col
 
-def generate_cont_features(data):
-    pass
+def create_age_bins(data, column, bins):
+    '''
+    Given a continuous variable, create a new column in the dataframe
+    that represents the bin in which the continuous variable falls into.
+
+
+    '''
+    new_col = 'bins_' + str(column)
+
+    data[new_col] = pd.cut(data[column], bins=bins)
+    #print pd.value_counts(data['age_group'])
+
+    return new_col
+
+def convert_to_binary(data, column, zero_string):
+    '''
+    Given a binary categorical variable, such as a gender column with
+    male and female, convert data to 0 for zero_string and 1 otherwise
+
+    Provide the string of the forthcoming 0 value, such as 'male', "MALE", or 'Male"
+    '''
+
+    data[column] = data[column].apply(lambda x: 0 if sex == zero_string else 1)
+
+def scale_column(data, column):
+
+    scaled_col = 'scaled_' + str(column)
+    data[scaled_col] = StandardScaler().fit_transform(data[column])
+
+    return scaled_col
 
 def model_data(training_data, test_data, features, label):
     
     model = LogisticRegression()
     model.fit(training_data[features], training_data[label])
-
-    #return model 
-
     predicted = model.predict(test_data[features])
-    print predicted
-    #model.score(test_data[features], test_data[label])
-    #print accuracy_score(predicted, test_data[label]) 
+    return predicted
 
-def evaluate_model(test_data, model):
-    pass
+def evaluate_model(test_data, label, predicted_values):
+    return accuracy_score(predicted_values, test_data[label]) 
 
-def go(training_file, testing_file):
+
+def go(training_file):
     '''
     run file
     '''
@@ -116,6 +140,18 @@ def go(training_file, testing_file):
     #print_statistics(df)
     #visualize(df)
 
+    '''
+    put in func
+    cols = ['NumberOfDependents', 'SeriousDlqin2yrs']
+    age_means = df[cols].groupby("NumberOfDependents").mean()
+    print age_means
+    age_means.plot()
+    plt.savefig('means.png')
+
+    df[["age_bucket", "serious_dlqin2yrs"]].groupby("age_bucket").mean()
+
+    '''
+
     # I'm imputting num dependents with 0 bc its the mode
     impute_missing_column(df, ['NumberOfDependents'], 'mode')
 
@@ -123,43 +159,51 @@ def go(training_file, testing_file):
     impute_missing_column(df, ['MonthlyIncome'], 'mean')
     #df['MonthlyIncome'] = df['MonthlyIncome'].fillna(int(df['MonthlyIncome'].median()))
 
-    print 'dataframe has no null values?:', not df.isnull().values.any()
-    print df.isnull().sum()
+    # print 'dataframe has no null values?:', not df.isnull().values.any()
+    # print df.isnull().sum()
 
     #log income
-    log_column(df, 'MonthlyIncome')
-    
+    new_log_col = log_column(df, 'MonthlyIncome')
+
+
+    mybins = [0] + range(20, 80, 5) + [120]
+    age_bucket = create_age_bins(df, 'age', mybins)
+
+
+    new_col = scale_column(df, 'MonthlyIncome')
+
+
     features = ['RevolvingUtilizationOfUnsecuredLines', 
                 'age', 'NumberOfTime30-59DaysPastDueNotWorse', 'DebtRatio', 'MonthlyIncome',
                 'NumberOfOpenCreditLinesAndLoans', 'NumberOfTimes90DaysLate', 
                 'NumberRealEstateLoansOrLines', 
                 'NumberOfTime60-89DaysPastDueNotWorse', 'NumberOfDependents']
-    label = 'SeriousDlqin2yrs'
 
+    #features = features + [new_log_col] + [age_bucket]
+    features = features + [new_log_col] + [new_col]
+
+    label = 'SeriousDlqin2yrs'
 
     # split train and test data
     train, test = train_test_split(df, test_size = 0.2)
 
+    predicted_values = model_data(train, test, features, label)
+    print evaluate_model(test, label, predicted_values)
 
-    #print model_data(train, test, features, label)
-    
-    #accuracy = evaluate_model(test, model)
-    #evaluate_model(training_file)
 
-    #print_value_counts(train, 'MonthlyIncome')
-    #print train['MonthlyIncome'].median()
+    # FIND BEST MIX OF FEATURES
 
-    # scaling 0 to 1
-    # convert binary male / female to 0 and 1
+    '''
+    Your task is to train one or more models on the training data and generate delinquency scores for the test data. 
+    '''
 if __name__=="__main__":
-    instructions = '''Usage: python workflow.py training_file, test_file'''
+    instructions = '''Usage: python workflow.py training_file'''
 
-    if(len(sys.argv) != 3):
+    if(len(sys.argv) != 2):
         print(instructions)
         sys.exit()
 
     training_file = sys.argv[1]
-    testing_file = sys.argv[2]
 
-    go(training_file, testing_file)
+    go(training_file)
 
