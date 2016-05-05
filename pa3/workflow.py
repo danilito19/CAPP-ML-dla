a40ff48f00390d0c39ab02928913337f6bac08f9
Dani <danalitovsky@gmail.com>
2016-05-05 08:08:06 -0500

    finished code

--- pa3/workflow.py
+++ pa3/workflow.py
@@ -248,6 +248,10 @@ def evaluate_model_threshold(test_data, label, predicted_values, threshold):
 
 def plot_precision_recall(y_true, y_prob, model_name, model_params):
 
+    '''
+    Plot a precision recall curve for one model with its y_prob values.
+    '''
+
     precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_prob)
     precision = precision_curve[:-1]
     recall = recall_curve[:-1]
@@ -263,10 +267,17 @@ def plot_precision_recall(y_true, y_prob, model_name, model_params):
     #plt.show()
 
 def plot_precision_recall_all_models(y_true, y_prob_dict, file_name):
+    '''
+    Plot precision-recall curves for models in the y_prob_dict
+
+    y_prob_dict has model names as keys and y_prob values as values
+
+    '''
 
     plt.clf()
 
     for model_name, y_prob in y_prob_dict.items():
+        print model_name, y_prob
         precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_prob)
         precision = precision_curve[:-1]
         recall = recall_curve[:-1]
@@ -284,6 +295,10 @@ def plot_precision_recall_all_models(y_true, y_prob_dict, file_name):
     #plt.show()
 
 def run_imputation(train, test, features):
+    '''
+    Impute feature columns for train data and use train-set statistics to impute
+    test data.
+    '''
 
     # impute TRAIN DATA dependents with mode
     train_dependents_mode = impute_missing_column(train, ['NumberOfDependents'], 'mode')
@@ -300,6 +315,10 @@ def run_imputation(train, test, features):
     assert not test.isnull().values.any()
 
 def generate_features(train, test, features):
+    '''
+    Generate features for train and test sets.
+
+    '''
 
     new_log_col = log_column(train, 'MonthlyIncome')
     age_bins = [0] + range(20, 80, 5) + [120]
@@ -310,8 +329,6 @@ def generate_features(train, test, features):
 
     scaled_income = scale_column(train, 'MonthlyIncome')
 
-
-    #log income
     new_log_col = log_column(test, 'MonthlyIncome')
     age_bucket = create_bins(test, 'age', age_bins)
     income_bucket = create_bins(test, 'MonthlyIncome', income_bins)
@@ -321,6 +338,12 @@ def generate_features(train, test, features):
 
 def model_loop(models_to_run, df, features, label, n_folds):
     '''
+    Given an array of models to run, a dataset, features, a label, and 
+    a number of folds, find the best model and its parameters among all the models and
+    the cross product of parameters using k-fold cross validation.
+
+    Returns values for the best model and a dictionary of y-predicted values for each
+    model to be used for plotting.
 
     '''
 
@@ -328,26 +351,18 @@ def model_loop(models_to_run, df, features, label, n_folds):
     best_overall_auc = 0
     best_overall_params = ''
 
-    #use a dict to save the y_prob values of models for plotting
-    y_prob_dict = {}
-    folds_auc_counter = 0
-
-
-    # create results-table csv
-    with open('results-table.csv', 'wb') as csvfile:
+    # create params-table csv
+    with open('parameters-table.csv', 'wb') as csvfile:
         w = csv.writer(csvfile, delimiter=',')
-        w.writerow(['MODEL', 'PARAMETERS', 'ACCURACY', 'PRECISION', 'RECALL', 'avg-fold-AUC', 'AUC-stdv'])
+        w.writerow(['MODEL', 'PARAMETERS', 'avg-fold-AUC', 'AUC-stdv'])
 
         for index,clf in enumerate([clfs[x] for x in models_to_run]):
             running_model = models_to_run[index]
             parameter_values = grid[running_model]
 
             top_intra_model_auc = 0
-            top_intra_model_params = ''
-
 
             for p in ParameterGrid(parameter_values):
-
                 auc_per_fold = []
 
                 kf = KFold(len(df), n_folds=n_folds)
@@ -362,13 +377,10 @@ def model_loop(models_to_run, df, features, label, n_folds):
                     clf.set_params(**p)
                     clf.fit(train[features], train[label])
 
-                    predicted_values = clf.predict(test[features])
                     if hasattr(clf, 'predict_proba'):
                         y_pred_probs = clf.predict_proba(test[features])[:,1] #second col only for class = 1
                     else:
                         y_pred_probs = clf.decision_function(test[features])
-
-                    accuracy, precision, recall, f1 = evaluate_model(test, label, predicted_values)
 
                     precision_curve, recall_curve, pr_thresholds = precision_recall_curve(test[label], y_pred_probs)
                     precision = precision_curve[:-1]
@@ -378,34 +390,30 @@ def model_loop(models_to_run, df, features, label, n_folds):
                     auc_per_fold.append(AUC)
 
                 avg_model_auc = np.mean(auc_per_fold)
-                auc_stdev = np.mean(auc_per_fold)
+                auc_stdev = np.std(auc_per_fold)
 
                 # find best parameters within a model and its y_pred to plot
                 if avg_model_auc > top_intra_model_auc:
                     top_intra_model_auc = avg_model_auc
-                    top_intra_model_params = clf
-                    top_intra_model_y_pred = y_pred_probs
-                    y_prob_dict[running_model] = top_intra_model_y_pred
+                    best_models[running_model] = p
 
-                w.writerow([running_model, clf, accuracy, precision, recall, avg_model_auc, auc_stdev])
+                w.writerow([running_model, clf, avg_model_auc, auc_stdev])
 
+            print 'FINISHED RUNNING MODEL %s' %running_model
             # find best model with params overall
             if avg_model_auc > best_overall_auc:
                 best_overall_auc = avg_model_auc
                 best_overall_model = running_model
                 best_overall_params = clf
 
-
-    return best_overall_model, best_overall_params, best_overall_auc, y_prob_dict
+    return best_overall_model, best_overall_params, best_overall_auc
 
 def go(training_file):
     '''
     Run functions for specific data file
     '''
     
-    ####### EXPLORE AND VISUALIZE ALL DATA
     df = read_data(training_file)
-    df = df[:500]
     
     #print_statistics(df)
     #visualize_all(df)
@@ -415,7 +423,6 @@ def go(training_file):
     #visualize_by_group_mean(df, [income_bucket, "SeriousDlqin2yrs"], income_bucket)
 
 
-    ######## GET FEATURES TO MODEL
     features = ['RevolvingUtilizationOfUnsecuredLines', 
                 'age', 'NumberOfTime30-59DaysPastDueNotWorse', 'DebtRatio', 'MonthlyIncome',
                 'NumberOfOpenCreditLinesAndLoans', 'NumberOfTimes90DaysLate', 
@@ -423,22 +430,55 @@ def go(training_file):
                 'NumberOfTime60-89DaysPastDueNotWorse', 'NumberOfDependents']
 
     label = 'SeriousDlqin2yrs'
+    
+    models_to_run=['LR','NB','DT','RF', 'SVM']
 
-    models_to_run=['LR','NB','DT', 'RF', 'SVM', 'GB']
+    #select folds for k fold validation
     n_folds = 3
 
     start_loop = time()
-    best_overall_model, best_overall_params, best_overall_auc, y_prob_dict = model_loop(models_to_run, df, features, label, n_folds)
+    best_overall_model, best_overall_params, best_overall_auc = model_loop(models_to_run, df, features, label, n_folds)
     loop_time_minutes = (time() - start_loop) / 60
+
     print 'LOOP THRU ALL MODELS TOOK %s MINUTES' % loop_time_minutes
     print 'BEST MODEL %s \n BEST PARAMS %s \n BEST AUC %s \n' % (best_overall_model, best_overall_params, best_overall_auc)
+    
 
+    # use entire dataset for train-testing on best model
+    train, test = train_test_split(df, test_size = 0.2)
+    run_imputation(train, test, features)
 
-    # plot precision-recall curve for all models (picking the best parameters of each model)
-    file_name = 'all_models.png'  
-    plot_precision_recall_all_models(test[label], y_prob_dict, file_name)
+    new_log_col, age_bucket, income_bucket, scaled_income = generate_features(train, test, features)
+    features = features + [new_log_col] + [age_bucket] + [income_bucket] + [scaled_income]
 
+    assert not test.isnull().values.any()
 
+    # create final-table csv
+    with open('final-table.csv', 'wb') as csvfile:
+        w = csv.writer(csvfile, delimiter=',')
+        w.writerow(['MODEL', 'PARAMETERS', 'ACCURACY', 'PRECISION', 'RECALL', 'AUC'])
+        params = best_overall_params
+        clf = clfs[best_overall_model]  
+        clf.set_params(**params)
+        clf.fit(train[features], train[label])
+        predicted_values = clf.predict(test[features])
+
+        accuracy, precision, recall, f1 = evaluate_model(test, label, predicted_values)
+
+        if hasattr(clf, 'predict_proba'):
+            y_pred_probs = clf.predict_proba(test[features])[:,1] #second col only for class = 1
+        else:
+            y_pred_probs = clf.decision_function(test[features])
+
+        precision_curve, recall_curve, pr_thresholds = precision_recall_curve(test[label], y_pred_probs)
+        pr = precision_curve[:-1]
+        rec = recall_curve[:-1]
+        AUC = auc(rec, pr)
+
+        w.writerow([model, params, accuracy, precision, recall, AUC])
+
+    # plot precision-recall of best model
+    plot_precision_recall(test[label], y_pred_probs, model, params)
 
 if __name__=="__main__":
     instructions = '''Usage: python workflow.py training_file'''
